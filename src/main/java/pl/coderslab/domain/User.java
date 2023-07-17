0c6aa59323faf61bf967c359bbae8f378f10240a
pawelkozolub <pkozolub@gmail.com>
2023-07-17 23:10:02 +0200

    userDao domain update > package name change

--- src/main/java/pl/coderslab/domain/User.java
+++ src/main/java/pl/coderslab/domain/User.java
@@ -1,4 +1,4 @@
-package pl.coderslab.entity;
+package pl.coderslab.domain;
 
 public class User {
     private int id;
--- src/main/java/pl/coderslab/domain/UserDao.java
+++ src/main/java/pl/coderslab/domain/UserDao.java
@@ -1,4 +1,4 @@
-package pl.coderslab.entity;
+package pl.coderslab.domain;
 
 import org.mindrot.jbcrypt.BCrypt;
 import pl.coderslab.utils.DbUtil;
